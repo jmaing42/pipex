ee2d168c4af974e058aeb26feaa5a19ef633c910
맹주영 <mjy9088@naver.com>
2022-09-04 20:06:02 +0900

    fix bug

--- src/pipex/pipex_new.c
+++ src/pipex/pipex_new.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 05:46:24 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/04 19:57:56 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/04 20:05:34 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,28 +18,28 @@
 #include "wrap.h"
 #include "ft_os_file.h"
 
-t_pipex	*pipex_new(size_t args_length, char **args)
+t_pipex	*pipex_new(size_t argc, char **argv)
 {
 	t_pipex	*result;
 	size_t	i;
 
-	if (args_length < 3)
+	if (argc < 3)
 		return (NULL);
-	result = wrap_malloc(
-			sizeof(t_pipex) + (args_length - 2) * sizeof(t_pipex_node));
-	result->node_count = args_length - 2;
-	result->in = ft_os_file_open(args[0], O_RDONLY);
-	result->out = ft_os_file_open2(args[0], O_WRONLY | O_CREAT | O_EXCL, 0644);
+	result = wrap_malloc(sizeof(t_pipex) + (argc - 2) * sizeof(t_pipex_node));
+	result->node_count = argc - 2;
+	result->in = ft_os_file_open(argv[0], O_RDONLY);
+	result->out = ft_os_file_open2(
+			argv[argc - 1], O_WRONLY | O_CREAT | O_EXCL, 0644);
 	i = 0;
-	while (++i < args_length - 1)
+	while (++i < argc - 1)
 	{
-		if (pipex_new_node(&result->node[i - 1], args[i]))
+		if (pipex_new_node(&result->node[i - 1], argv[i]))
 		{
 			result->node_count = i - 1;
 			break ;
 		}
 	}
-	if (result->node_count != args_length - 2)
+	if (result->node_count != argc - 2)
 	{
 		pipex_free(result);
 		return (NULL);
