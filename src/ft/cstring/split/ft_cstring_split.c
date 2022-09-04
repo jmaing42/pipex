0fb15b352f35524cfaefffffeafef750b8864a35
맹주영 <mjy9088@naver.com>
2022-09-04 21:36:26 +0900

    fix split bug

--- src/ft/cstring/split/ft_cstring_split.c
+++ src/ft/cstring/split/ft_cstring_split.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 05:37:46 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/04 18:51:55 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/04 21:35:27 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,37 @@
 #include "wrap.h"
 #include "ft_cstring.h"
 
+typedef struct s_ft_cstring_split
+{
+	size_t		index;
+	bool		alloc_failed;
+	const char	*tmp;
+}	t_ft_cstring_split;
+
 char	**ft_cstring_split(const char *str, const char *charset)
 {
-	const size_t	len = ft_cstring_split_count(str, charset);
-	char **const	result = (char **)wrap_malloc(sizeof(char *) * (len + 1));
-	size_t			index;
-	bool			alloc_failed;
-	const char		*tmp;
+	t_ft_cstring_split	l;
+	const size_t		len = ft_cstring_split_count(str, charset);
+	char **const		result
+		= (char **)wrap_malloc(sizeof(char *) * (len + 1));
 
+	l.alloc_failed = false;
 	if (!result)
 		return (NULL);
 	result[len] = NULL;
-	index = 0;
+	l.index = 0;
 	str = ft_cstring_find_one_of(str, charset, true);
 	while (*str)
 	{
-		tmp = ft_cstring_find_one_of(str, charset, false);
-		result[index] = ft_cstring_duplicate_length(str, tmp - str);
-		if (!result[index++])
-			alloc_failed = true;
-		if (!*tmp || alloc_failed)
+		l.tmp = ft_cstring_find_one_of(str, charset, false);
+		result[l.index] = ft_cstring_duplicate_length(str, l.tmp - str);
+		if (!result[l.index++])
+			l.alloc_failed = true;
+		if (!*l.tmp || l.alloc_failed)
 			break ;
-		str = ft_cstring_find_one_of(tmp, charset, true);
+		str = ft_cstring_find_one_of(l.tmp, charset, true);
 	}
-	if (!alloc_failed)
+	if (!l.alloc_failed)
 		return (result);
 	ft_cstring_split_free(result);
 	return (NULL);
