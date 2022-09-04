56dc6f695b0ff422fcd18f6106b8a744dc04d078
맹주영 <mjy9088@naver.com>
2022-09-04 22:05:46 +0900

    WIP: add ft_os_fork_multiple

--- src/ft/os/fork/ft_os_fork_multiple.c
+++ src/ft/os/fork/ft_os_fork_multiple.c
@@ -1,23 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_os_fork.h                                       :+:      :+:    :+:   */
+/*   ft_os_fork_multiple.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/04 19:41:08 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/04 21:53:37 by Juyeong Maing    ###   ########.fr       */
+/*   Created: 2022/09/04 21:52:22 by Juyeong Maing     #+#    #+#             */
+/*   Updated: 2022/09/04 22:05:27 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_OS_FORK_H
-# define FT_OS_FORK_H
+#include "ft_os_fork.h"
 
-# include <unistd.h>
+t_err	ft_os_fork_multiple(size_t count, pid_t *out, size_t *out_index)
+{
+	size_t	i;
 
-# include "ft_types.h"
-
-t_err	ft_os_fork(pid_t *out_pid);
-t_err	ft_os_fork_multiple(size_t count, pid_t *out, size_t *out_index);
-
-#endif
+	i = -1;
+	while (++i < count)
+	{
+		if (ft_os_fork(&out[i]))
+		{
+			return (true); // TODO: kill all child processes
+		}
+		if (out[i] == 0)
+		{
+			*out_index = i;
+			return (false);
+		}
+	}
+	*out_index = -1;
+	return (false);
+}
