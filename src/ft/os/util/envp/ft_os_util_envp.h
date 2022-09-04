3f61eabbcdc26410c933f82fc9d959002a864d52
맹주영 <mjy9088@naver.com>
2022-09-04 15:37:50 +0900

    change ft_os_environment to to_os_util_envp

---
+++ src/ft/os/util/envp/ft_os_util_envp.h
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_os_util_envp.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/04 15:08:56 by Juyeong Maing     #+#    #+#             */
+/*   Updated: 2022/09/04 15:37:24 by Juyeong Maing    ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_OS_UTIL_ENVP_H
+# define FT_OS_UTIL_ENVP_H
+
+# include "ft_types.h"
+# include "ft_simple_map.h"
+
+extern char	**environ;
+
+typedef struct s_ft_os_util_envp_builder
+{
+	t_ft_simple_map_static	*map;
+}	t_ft_os_util_envp_builder;
+
+t_ft_os_util_envp_builder	*new_ft_os_util_envp_builder(
+								char **environ);
+t_ft_os_util_envp_builder	*ft_os_util_envp_builder_clone(
+								t_ft_os_util_envp_builder *builder);
+
+char						**ft_os_util_envp_builder_free(
+								t_ft_os_util_envp_builder *self);
+
+t_err						ft_os_util_envp_builder_override(
+								t_ft_os_util_envp_builder *self,
+								char **environ);
+t_err						ft_os_util_envp_builder_override_entry(
+								t_ft_os_util_envp_builder *self,
+								char *key,
+								char *value);
+t_err						ft_os_util_envp_builder_override_builder(
+								t_ft_os_util_envp_builder *self,
+								t_ft_os_util_envp_builder *builder);
+
+char						**ft_os_util_envp_builder_build(
+								t_ft_os_util_envp_builder *self);
+
+char						*ft_os_util_envp_builder_get(
+								t_ft_os_util_envp_builder *self,
+								char *key);
+
+char						*ft_os_util_envp_get(
+								char **environ,
+								char *key);
+
+#endif
--- src/pipex/pipex_new_node.c
+++ src/pipex/pipex_new_node.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 06:10:23 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/04 15:28:25 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/04 15:34:48 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 #include "ft_cstring.h"
 #include "ft_cstring_split.h"
 #include "ft_os_pipe.h"
-#include "ft_os_environment.h"
+#include "ft_os_util_envp.h"
 
 char	**init_environment(char **environ)
 {
-	t_ft_os_environment_builder *const	builder
-		= new_ft_os_environment_builder(environ);
+	t_ft_os_util_envp_builder *const	builder
+		= new_ft_os_util_envp_builder(environ);
 	char								**result;
 
 	if (!builder)
 		return (NULL);
-	result = ft_os_environment_builder_build(builder);
-	ft_os_environment_builder_free(builder);
+	result = ft_os_util_envp_builder_build(builder);
+	ft_os_util_envp_builder_free(builder);
 	return (result);
 }
 
