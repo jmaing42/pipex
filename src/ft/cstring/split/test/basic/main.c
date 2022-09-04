e435a7f8bd2c785782806987c5b508946c436f0c
맹주영 <mjy9088@naver.com>
2022-09-04 21:15:19 +0900

    add split test case

--- src/ft/cstring/split/test/basic/main.c
+++ src/ft/cstring/split/test/basic/main.c
@@ -6,7 +6,7 @@
 /*   By: Juyeong Maing <jmaing@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 20:40:45 by Juyeong Maing     #+#    #+#             */
-/*   Updated: 2022/09/04 21:06:51 by Juyeong Maing    ###   ########.fr       */
+/*   Updated: 2022/09/04 21:14:46 by Juyeong Maing    ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,17 @@
 int	main(void)
 {
 	char **const	s1 = ft_cstring_split("Hello, world!\n", ", \n");
+	char **const	s2 = ft_cstring_split("\fa\tb\r\nc\v", "\t\n\v\f\r ");
 
 	assert_not_null(s1);
 	assert_str_eq(s1[0], "Hello");
 	assert_str_eq(s1[1], "world!");
 	assert_null(s1[2]);
+	ft_cstring_split_free(s1);
+	assert_not_null(s2);
+	assert_str_eq(s2[0], "a");
+	assert_str_eq(s2[1], "b");
+	assert_str_eq(s2[2], "c");
+	assert_null(s2[3]);
+	ft_cstring_split_free(s2);
 }
