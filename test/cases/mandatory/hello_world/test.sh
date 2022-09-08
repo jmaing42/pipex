5b6c4cfbc1b4304f0de3a3f4d3c12aad14d1f76a
맹주영 <mjy9088@naver.com>
2022-09-08 23:51:48 +0900

    add test

--- test/cases/mandatory/hello_world/test.sh
+++ test/cases/mandatory/hello_world/test.sh
@@ -4,5 +4,9 @@ set -e
 
 rm -f "$(dirname "$0")/result.txt"
 ../tmp/pipex "$(dirname "$0")/in.txt" "cat" "cat" "$(dirname "$0")/result.txt"
-cmp -s "$(dirname "$0")/result.txt" "$(dirname "$0")/out.txt"
+cmp -s "$(dirname "$0")/result.txt" "$(dirname "$0")/out-1.txt"
+../tmp/pipex "$(dirname "$0")/in.txt" "cat" "cat" "$(dirname "$0")/result.txt"
+cmp -s "$(dirname "$0")/result.txt" "$(dirname "$0")/out-1.txt"
+../tmp/pipex "$(dirname "$0")/in.txt" "grep Bye" "wc -l" "$(dirname "$0")/result.txt"
+cmp -s "$(dirname "$0")/result.txt" "$(dirname "$0")/out-2.txt"
 rm -f "$(dirname "$0")/result.txt"
