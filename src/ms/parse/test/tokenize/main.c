199406373f9cf0d1798718a0253032a997588873
맹주영 <mjy9088@naver.com>
2022-10-19 07:23:14 +0900

    add leak test for tokenizing

--- src/ms/parse/test/tokenize/main.c
+++ src/ms/parse/test/tokenize/main.c
@@ -15,6 +15,7 @@
 
 #include "ft_types.h"
 #include "ms_parse.h"
+#include "ft/leak_test.h"
 
 static t_err	read_file_contents(const char *filename, char **out)
 {
@@ -72,41 +73,69 @@ static t_err	print_token_type(t_ms_parse_token_type type)
 	return (true);
 }
 
-static t_err	print_token(t_ms_parse_token token)
+static t_err	print_token_list(t_ms_parse_token_list list)
 {
-	if (print_token_type(token.type))
-		return (true);
-	if (token.type == MS_PARSE_TOKEN_TYPE_WORD
-		|| token.type == MS_PARSE_TOKEN_TYPE_WORD_QUOTED
-		|| token.type == MS_PARSE_TOKEN_TYPE_WORD_DOUBLE_QUOTED)
-		return (printf(" - %s\n", token.data) < 0);
-	else
-		return (printf("\n") < 0);
+	t_ms_parse_token_list_node	*node;
+	t_ms_parse_token			token;
+
+	node = list.head;
+	while (node)
+	{
+		token = node->value;
+		if (print_token_type(token.type))
+			return (true);
+		if (token.type == MS_PARSE_TOKEN_TYPE_WORD
+			|| token.type == MS_PARSE_TOKEN_TYPE_WORD_QUOTED
+			|| token.type == MS_PARSE_TOKEN_TYPE_WORD_DOUBLE_QUOTED)
+		{
+			if (printf(" - %s\n", token.data) < 0)
+				return (true);
+		}
+		else
+		{
+			if (printf("\n") < 0)
+				return (true);
+		}
+		node = node->next;
+	}
+	return (false);
+}
+
+static bool	test_leak(const void *context)
+{
+	const char *const		contents = context;
+	t_ms_parse_token_list	list;
+
+	leak_test_start();
+	if (!ms_parse_tokenize(contents, &list))
+		ms_parse_tokenize_free(&list);
+	return (false);
 }
 
 int	main(int argc, char **argv)
 {
-	char						*contents;
-	t_ms_parse_token_list		list;
-	t_ms_parse_token_list_node	*node;
+	char					*contents;
+	t_ms_parse_token_list	list;
+	int						errno;
+	t_err					error;
 
 	if (argc < 2 || read_file_contents(argv[1], &contents))
-		return (true);
+		return (EXIT_FAILURE);
+	errno = leak_test(test_leak, contents, NULL);
+	if (errno && printf("leak_test: %s\n", leak_test_error(errno)) < 0)
+	{
+		free(contents);
+		return (EXIT_FAILURE);
+	}
+	error = !!errno;
 	if (ms_parse_tokenize(contents, &list))
 	{
 		free(contents);
 		return (EXIT_FAILURE);
 	}
-	node = list.head;
-	while (node)
-	{
-		if (print_token(node->value))
-		{
-			ms_parse_tokenize_free(&list);
-			return (EXIT_FAILURE);
-		}
-		node = node->next;
-	}
+	error |= print_token_list(list);
 	ms_parse_tokenize_free(&list);
+	if (error)
+		return (EXIT_FAILURE);
 	return (EXIT_SUCCESS);
 }
