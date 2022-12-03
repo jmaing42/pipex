842c0542bea03ac0816c35845a53cc83716155d6
맹주영 <mjy9088@naver.com>
2022-12-04 02:27:37 +0900

    fix test error

--- test/test.sh
+++ test/test.sh
@@ -12,7 +12,7 @@ cd "$1"
 while IFS="," read -r input cmd
 do
   sh -c "../../../tmp/pipex $input \"$(echo $cmd | sed -e "s/,/\" \"/g")\" tmp.$$.txt";
-  sh -c "< $input $(echo $cmd | sed -e "s/,/ | /g")" | cmp -s tmp.$$.txt;
+  sh -c "< $input $(echo $cmd | sed -e "s/,/ | /g")" | cmp -s - tmp.$$.txt;
   rm -f tmp.$$.txt
   if [ "$V" = "1" ] || [ "$V" = "2" ] || [ "$V" = "3" ]; then
     if [ -t 1 ]; then
