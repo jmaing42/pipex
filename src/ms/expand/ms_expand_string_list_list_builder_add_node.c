d8714b97108cfbffc04e16de7bede389a5a04663
seonlim <dltjwl1@naver.com>
2023-03-27 22:39:16 +0900

    ms_expand_string_list_list_builder_feed_word: node

--- src/ms/expand/ms_expand_string_list_list_builder_add_node.c
+++ src/ms/expand/ms_expand_string_list_list_builder_add_node.c
@@ -6,7 +6,7 @@
 /*   By: seonlim <seonlim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 20:05:40 by seonlim           #+#    #+#             */
-/*   Updated: 2023/03/27 21:59:01 by seonlim          ###   ########.fr       */
+/*   Updated: 2023/03/27 22:12:03 by seonlim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,19 @@
 #include "ft_stringbuilder.h"
 #include "ft_types.h"
 #include "ms_execute.h"
+#include "ms_expand.h"
 
 static t_err	fill_node(
 	t_stringbuilder *builder,
-	t_ms_expand_string_list_list_node *node
+	t_ms_expand_string_list_node *node
 )
 {
 	char	*str;
+
+	str = stringbuilder_to_string(builder, 0);
+	if (str == NULL)
+		return (true);
+	node->str = str;
 	return (false);
 }
 
@@ -28,21 +34,23 @@ t_err	ms_expand_string_list_list_builder_add_node(
 	t_ms_expand_string_list_list_builder *self
 )
 {
-	t_ms_expand_string_list_list_node	*new_node;
+	t_ms_expand_string_list			*list;
+	t_ms_expand_string_list_node	*new_node;
 
-	new_node = ft_memory_allocate(1, sizeof(t_ms_expand_string_list_list_node));
+	list = &self->list.tail->list;
+	new_node = ft_memory_allocate(1, sizeof(t_ms_expand_string_list_node));
 	if (new_node == NULL)
 		return (true);
 	fill_node(self->builder, new_node);
-	if (self->list.head == NULL)
+	if (list->head == NULL)
 	{
-		self->list.head = new_node;
-		self->list.tail = new_node;
+		list->head = new_node;
+		list->tail = new_node;
 	}
 	else
 	{
-		self->list.tail->next = new_node;
-		self->list.tail = new_node;
+		list->tail->next = new_node;
+		list->tail = list->tail->next;
 	}
 	return (false);
 }
--- src/ms/expand/ms_expand_string_list_list_builder_feed_word.c
+++ src/ms/expand/ms_expand_string_list_list_builder_feed_word.c
@@ -1,15 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fake_file_name (file name is useless too)          :+:      :+:    :+:   */
+/*   ms_expand_string_list_list_builder_feed_wor        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: 42header-remover <whatever@example.com>    +#+  +:+       +#+        */
+/*   By: seonlim <seonlim@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 1970/01/01 00:00:00 by VCS handles       #+#    #+#             */
-/*   Updated: 1970/01/01 00:00:00 by file history     ###   ########.fr       */
+/*   Updated: 2023/03/27 22:38:54 by seonlim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_memory.h"
 #include "ms_expand.h"
 
 #include "ms.h"
@@ -19,10 +20,23 @@ t_err	ms_expand_string_list_list_builder_feed_word(
 	t_ms_word *word
 )
 {
-	return (
-		ms_expand_string_list_list_builder_feed_word_part_list(
-			self,
-			&word->part_list
-		)
-	);
+	t_ms_expand_string_list_list_node	*node;
+
+	node = ft_memory_allocate(1, sizeof(t_ms_expand_string_list_list_node));
+	if (node == NULL)
+		return (true);
+	if (self->list.head == NULL)
+	{
+		self->list.head = node;
+		self->list.tail = node;
+	}
+	else
+	{
+		self->list.tail->next = node;
+		self->list.tail = self->list.tail->next;
+	}
+	if (ms_expand_string_list_list_builder_feed_word_part_list(
+		self, &word->part_list))
+		return (true);
+	return (false);
 }
